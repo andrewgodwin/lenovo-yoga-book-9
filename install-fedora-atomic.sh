da6e5dc99b9a3dbc3efc403a3c92d52f07093fb5
Andrew Godwin <andrew@aeracode.org>
2026-01-19 09:49:12 -0700

    Add modprobe rule to ignore top touch device

--- install-fedora-atomic.sh
+++ install-fedora-atomic.sh
@@ -1,8 +1,11 @@
 # Add kernel args
 sudo rpm-ostree kargs --append=pci=hpiosize=0 --append=video=eDP-1:panel_orientation=upside_down
 
+# Add udev rule to ignore the default touch device
+sudo cp udev/99-lenovo-yoga-book-9.rules /etc/udev/rules.d/
+
 # Add HWDB quirk for top screen coordinates
-sudo cp 99-lenovo-yoga-book-9.hwdb /etc/udev/hwdb.d/99-lenovo-yoga-book-9.hwdb
+sudo cp hwdb/99-lenovo-yoga-book-9.hwdb /etc/udev/hwdb.d/
 sudo systemd-hwdb update
 
 # Install and enable the touchscreen daemon
